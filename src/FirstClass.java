48688f3bc3a6050cb80c543f6bb401530d1ea9f7
tseiitbekov <temirlan.seiitbekov@diabolocom.com>
2022-11-11 14:04:17 +0600

    First commit

---
+++ src/FirstClass.java
@@ -0,0 +1,10 @@
+public class FirstClass {
+
+    public static void main(String[] args) {
+        printFirstClass();
+    }
+
+    public static void printFirstClass() {
+        System.out.println("Print first class");
+    }
+}
