0e061cca7ea3f59179873ba941c2d0644f836b2e
JasurKadyrov <jk.kadyrov@gmail.com>
2022-11-12 11:09:46 -0800

    My second commit

--- src/ThirdClass.java
+++ src/ThirdClass.java
@@ -1,5 +1,7 @@
 public class ThirdClass {
     public static void main(String[] args) {
         System.out.println("Change from Jasur");
+
+        System.out.println("This is a new change from Jasur");
     }
 }
