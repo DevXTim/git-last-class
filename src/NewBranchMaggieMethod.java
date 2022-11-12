49eb5040ee4199bd513a1aecef18e84242c5b549
maggiee0606 <maggiee0606@yahoo.com>
2022-11-11 22:42:48 -0600

    New changes to my main method

--- src/NewBranchMaggieMethod.java
+++ src/NewBranchMaggieMethod.java
@@ -1,7 +1,9 @@
 public class NewBranchMaggieMethod {
     public static void main(String[] args) {
-        String name = "Hello, it is Maggie. Nice to meet you. ";
-        System.out.println(newBranchMaggie(name));
+        String greeting1 = "Hello, I'm Maggie.";
+        String greeting2 = "Nice to meet you.";
+        System.out.println(newBranchMaggie(greeting1));
+        System.out.println(newBranchMaggie(greeting2));
     }
 
     public static String newBranchMaggie(String name) {
