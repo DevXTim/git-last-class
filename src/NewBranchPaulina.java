0aee672b3d6be22b5f47179b07f28702949f6f0a
paulinagora88 <paulinagora@yahoo.com>
2022-11-11 22:03:00 -0600

    New change to value

---
+++ src/NewBranchPaulina.java
@@ -0,0 +1,12 @@
+public class NewBranchPaulina {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println(printValue(num));
+
+    }
+
+
+    static int printValue(int num) {
+        return(num);
+    }
+}
