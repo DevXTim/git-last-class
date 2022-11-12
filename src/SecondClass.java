169a44ed76001c38e81ea17dfa657f1954bd96a4
gbaktybek <baktybekovaguldana47@gmail.com>
2022-11-11 18:57:54 -0600

    Second commit - added count method

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -2,5 +2,12 @@ public class SecondClass {
 
     public static void main(String[] args) {
         System.out.println("Print second class");
+        printNumbers(7);
+    }
+
+    public static void printNumbers(int nums){
+        for ( int i= 0; i < nums; i++){
+            System.out.println(i);
+        }
     }
 }
