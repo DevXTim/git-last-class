3b2f6e407b230a6a6ff949c1903d67fb314ba317
Josh <joshuasevendavid@gmail.com>
2022-11-11 21:28:20 -0500

    hackathon

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -2,7 +2,7 @@ public class SecondClass {
 
     public static void main(String[] args) {
         System.out.println("Print second class");
-
+        System.out.println("You have been hacked! ");
         printNumbers(15);
     }
 
