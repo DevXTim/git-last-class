73c4f5610eb839a115e8cefe9464827d5e5119f3
maggiee0606 <maggiee0606@yahoo.com>
2022-11-11 22:57:52 -0600

    Next change

--- src/NewBranchMaggieMethod.java
+++ src/NewBranchMaggieMethod.java
@@ -1,11 +1,16 @@
 public class NewBranchMaggieMethod {
     public static void main(String[] args) {
         String name = "Hello, it is Maggie. Nice to meet you. ";
+        int num=33;
         System.out.println(newBranchMaggie(name));
+        System.out.println("I am "+ MaggieSecondBranch(num)+" years old.");
     }
 
     public static String newBranchMaggie(String name) {
         return name;
+    }
+    public static int MaggieSecondBranch (int num){
+        return num;
     }
     }
 
