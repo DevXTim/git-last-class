56e250a692f6b5815ac2452e02272dc6b257586f
cherry7859 <cmfinley97@gmail.com>
2022-11-12 10:51:19 -0600

    Changes from Cherry

---
+++ src/CherryClass.java
@@ -0,0 +1,9 @@
+public class CherryClass {
+    public static void main(String[] args) {
+        cherryChanges();
+    }
+
+    static void cherryChanges(){
+        System.out.println("Changes from Cherry");
+    }
+}
