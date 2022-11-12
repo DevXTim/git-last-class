5ea451022d3b367b11c805d43b425886d96a54b6
tseiitbekov <temirlan.seiitbekov@diabolocom.com>
2022-11-12 07:36:01 +0600

    Fourth commit

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -7,4 +7,8 @@ public class FirstClass {
     public static void printFirstClass() {
         System.out.println("Print first class");
     }
+
+    public static void doNothing() {
+        System.out.println("");
+    }
 }
