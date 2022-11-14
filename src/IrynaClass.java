6c0cda0da876c0d3ef662f3dc9447fe6c0dc7efd
livairyn <111402211+livairyn@users.noreply.github.com>
2022-11-14 18:48:58 -0500

    Second commit

--- src/IrynaClass.java
+++ src/IrynaClass.java
@@ -1,8 +1,10 @@
 public class IrynaClass {
     public static void main(String[] args) {
-        int nums[] = {15, 18, 44, 32, 75, 8, 35, 78, 10};
+        int nums[] = {15, 18, 44, 32, 75, 8, 35, 78, 10, 54, 75,21};
         int greatestNum = 0;
+        int value = 0;
 
+        System.out.println(calculateSum(nums, value));
         System.out.println(findGreatestNum(nums, greatestNum));
     }
 
@@ -15,6 +17,17 @@ public class IrynaClass {
         }
         return greatestNum;
     }
+    public static int calculateSum(int[] nums, int value){
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++){
+
+            sum = sum + nums[i];
+
+        }
+        return sum;
+
+}
 }
 
 
