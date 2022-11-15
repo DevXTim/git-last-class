4260c15e84cb8944115b1eb935fcdb31fac3f2c8
nytka1989 <nytkaarsen@gmail.com>
2022-11-15 18:05:34 -0500

    My change

--- src/ArsensFile.java
+++ src/ArsensFile.java
@@ -2,6 +2,9 @@ public class ArsensFile {
 
     public static void main(String[] args) {
         System.out.println("Greetings!");
+
+
+        System.out.println("Struggling with Git");
     }
 
 }
