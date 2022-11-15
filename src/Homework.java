941c49f6f6e762c637dcd52923d48820b94f3dad
Ali Aykilinc <aliugura@Alis-MacBook-Air.local>
2022-11-15 02:13:30 -0500

    Alis Homework

---
+++ src/Homework.java
@@ -0,0 +1,20 @@
+public class Homework {
+    public static void main(String[] args) {
+
+
+
+        printNumber(10);
+
+    }
+
+    public static void printNumber(int number){
+            for  (int i=0; i <= number; i++){
+
+                System.out.println(i);
+
+            }
+
+        }
+    }
+
+
