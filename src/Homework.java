a28e5459eb797c4b547b077a121c1c1ac6334176
Ali Aykilinc <aliugura@Alis-MacBook-Air.local>
2022-11-15 02:24:04 -0500

    Alis Homework Part 2

--- src/Homework.java
+++ src/Homework.java
@@ -1,7 +1,6 @@
 public class Homework {
     public static void main(String[] args) {
-
-
+        System.out.println("Hello git. You are really complex, but i will learn you!");
 
         printNumber(10);
 
