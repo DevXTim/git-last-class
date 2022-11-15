de20006b591ba504356e1ed88a199ef3693e7121
irinaapollo88 <irinaapollo88@gmail.com>
2022-11-15 13:16:04 -0500

    My second commit in FirstClass

--- src/IrinasClass.java
+++ src/IrinasClass.java
@@ -6,6 +6,7 @@ public class IrinasClass {
         for (int i = 0; i <= num; i++) {
             System.out.println(i);
             System.out.println("It is interesting");
+            System.out.println("Please review this file");
             }
         }
     }
