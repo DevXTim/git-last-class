f337a4c5443eb9853d00f7866caaec75e665cc48
irinaapollo88 <irinaapollo88@gmail.com>
2022-11-15 12:01:42 -0500

    My first commit

---
+++ src/IrinasClass.java
@@ -0,0 +1,11 @@
+public class IrinasClass {
+    public static void main(String[] args) {
+        int num = 35;
+
+
+        for (int i = 0; i <= num; i++) {
+            System.out.println(i);
+            }
+        }
+    }
+
