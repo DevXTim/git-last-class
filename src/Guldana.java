b80c278658e095dcacebe3ca4f2270190452aa6f
gbaktybek <baktybekovaguldana47@gmail.com>
2022-11-16 01:00:04 -0600

    new commit

--- src/Guldana.java
+++ src/Guldana.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Guldana {
+public class Guldana {
+    public static void main(String[] args) {
+        String name = "Guldana";
+        printName(name);
+
+    }
+    static void printName(String name){
+        System.out.println("My name is " + name);
+    }
 }
