f479fe3f8c2818d552700bef20e9a3143f4717b7
gbaktybek <baktybekovaguldana47@gmail.com>
2022-11-16 01:12:47 -0600

    add another method

--- src/Guldana.java
+++ src/Guldana.java
@@ -3,8 +3,13 @@ public class Guldana {
         String name = "Guldana";
         printName(name);
 
+        String greet = "Hello";
+        sayHi(greet);
     }
     static void printName(String name){
         System.out.println("My name is " + name);
+    }
+    static void sayHi(String greet){
+        System.out.println(greet + " , how are you?");
     }
 }
