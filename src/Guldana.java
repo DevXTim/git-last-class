64b5ecedcd22d62b7e6d1e9374cca1f84adf1bd3
gbaktybek <baktybekovaguldana47@gmail.com>
2022-11-16 15:47:13 -0600

    add printName method

---
+++ src/Guldana.java
@@ -0,0 +1,10 @@
+public class Guldana {
+    public static void main(String[] args) {
+
+        String name = "Guldana";
+        printName(name);
+    }
+    static void printName(String name){
+        System.out.println("My name is "+ name);
+    }
+}
