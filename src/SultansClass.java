f2f5592169642324f2c44e9d5e49dc6e0a03d5a4
Sultan-Git <ssultan102511@gmail.com>
2022-11-17 13:33:12 -0500

    Sultan's class

--- src/SultansClass.java
+++ src/SultansClass.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class SultansClass {
+import java.util.Scanner;
+
+public class SultansClass {
+    public static void main(String[] args) {
+        System.out.println("Sultan's changes");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+    }
+    public static int countTheLength(String str){
+     if (str.length()>0){
+         return str.length();
+     }else {
+         return -1;
+     }
+    }
 }
