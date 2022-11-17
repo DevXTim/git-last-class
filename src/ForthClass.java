d96d28ffcceb83e46ce706d57a095a9fba43bc13
unknown <khrystynadulyk@gmail.com>
2022-11-17 12:46:32 -0600

    Eight commit

--- src/ForthClass.java
+++ src/ForthClass.java
@@ -1,5 +1,10 @@
 public class ForthClass {
     public static void main(String[] args) {
         System.out.println("Magic is happening now");
+        int data[] = {12, 23, 43, 23, 34, 12};
+        for(int i =0; i< data.length; i++){
+            System.out.println(data[i]);
+        }
+
     }
 }
