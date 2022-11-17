32ab7549f0b444d93606c3eedd5100360d42a76b
gbaktybek <baktybekovaguldana47@gmail.com>
2022-11-16 20:57:32 -0600

    created new class and add method

---
+++ src/Guldana01.java
@@ -0,0 +1,10 @@
+public class Guldana01 {
+    public static void main(String[] args) {
+        String name = "GUldana";
+        printName(name);
+
+    }
+    static void printName(String name){
+        System.out.println("My name is " + name);
+    }
+}
