d14bd64906cdf9917ac11f9b82129dae8f5ba3e3
lucekdj <lucekdj@gmail.com>
2022-11-18 10:51:47 -0600

    added new sout ...Second Branch Created Nov 18

--- src/JuliusNewBranch.java
+++ src/JuliusNewBranch.java
@@ -4,5 +4,6 @@ public class JuliusNewBranch {
         System.out.println(" Hello Java ");
         System.out.println("Hello Warld");
         System.out.println("Today is Nov 18 2022 ");
+        System.out.println("Second Branch Created Nov 18");
     }
 }
