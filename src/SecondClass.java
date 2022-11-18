7d3f94becd092d06b3a613e8591fb55cc1d60fb6
Gentian Brahimaj <gentianbrahimaj1@gmail.com>
2022-11-18 17:03:56 -0600

    New changes made first commit

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -3,6 +3,8 @@ public class SecondClass {
     public static void main(String[] args) {
         System.out.println("Print second class");
         System.out.println("You have been hacked! ");
+        System.out.println("Not really, but you should be careful with your code");
+
         printNumbers(15);
     }
 
