0ed765bdd97e773991871f387a06dcd684420e1d
Tetiana Vlokh <taniavlokh0204@gmail.com>
2022-11-20 12:31:03 -0600

    Third commit

--- src/TetianaVlMethod.java
+++ src/TetianaVlMethod.java
@@ -1,5 +1,17 @@
 public class TetianaVlMethod {
     public static void main(String[] args) {
         System.out.println("Hello people");
+
+
+        System.out.println(overloaded(3,6,7));
     }
-}
+
+    public static int overloaded(int a, int b, int c) {
+        return a*b*c;
+    }
+
+    }
+
+
+
+
