11139565740cafadbe00792ed78a3f84893a7b00
MariaOsokina <maria.tokareva@gmail.com>
2022-11-25 16:55:29 -0600

    Changed sout to Hello Everyone again and Have a great day

--- src/MariaOsokina.java
+++ src/MariaOsokina.java
@@ -3,6 +3,7 @@ public class MariaOsokina {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Everyoune again");
+        System.out.println("Hello Everyone again");
+        System.out.println("Have a great day!");
     }
 }
