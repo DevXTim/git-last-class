77f13fcb94db525b18309834050d5134046b660c
sophiyanoor <sofianurgali@gmail.com>
2022-11-26 19:05:16 -0500

    Sophia Branch

---
+++ src/NewBranchSophia.java
@@ -0,0 +1,13 @@
+public class NewBranchSophia {
+    public static void main(String[] args) {
+        System.out.println("Hello everyone! -from Sophia");
+
+        int a = 7;
+        int b = 2;
+        System.out.println(addTwoNumbers(a, b));
+    }
+    public static int addTwoNumbers (int a, int b) {
+        int sum = a+b;
+        return sum;
+    }
+}
