6c5b53038cb362ea4557697043554a25d278cc71
moistakan@gmail.com <Karina>
2022-11-27 20:52:18 -0800

    Karina's new branch with changes

--- src/Karina.java
+++ src/Karina.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 public class Karina {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please enter your name:");
+        String name=sc.nextLine();
 
         christmasTree();
+        newYearWish(name);
+
     }
     public static void christmasTree(){
         for(int i=1; i<=5; i++){
@@ -22,5 +28,13 @@ public class Karina {
             }
             System.out.println();
         }
+    }
+    public static void newYearWish(String name){
+        System.out.println("Dear " + name + ",");
+        System.out.println("From the bottom of my heart, I wish you much success in pursuing your dreams.");
+        System.out.println("A New Year is like a blank book, and the pen is in your hands.");
+        System.out.println("It is your chance to write a beautiful story for yourself.");
+        System.out.println("Always remember, YOU ARE AMAZING!");
+        System.out.println("Happy New Year!");
     }
 }
