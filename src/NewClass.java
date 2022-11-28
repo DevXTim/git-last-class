02fa6f21bdc493e74a8bc745d5b80aa8c0b55b39
nurakim <nu.rare7@gmail.com>
2022-11-28 15:28:27 -0800

    last commit

--- src/NewClass.java
+++ src/NewClass.java
@@ -1,6 +1,7 @@
 public class NewClass {
     public static void main(String[] args) {
         System.out.println("Hello everyone");
+        System.out.println("Thank you for being here");
 
     }
 
